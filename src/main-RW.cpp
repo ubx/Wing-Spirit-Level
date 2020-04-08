818c3f2b1d05e0e498b2a3d986aeafe37107052b
andreas <andreas.luethi@gmx.net>
2020-04-08 09:25:36 +0200

    Display only difference.

--- src/main-RW.cpp
+++ src/main-RW.cpp
@@ -43,6 +43,7 @@ void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
 
     M5.IMU.getAccelData(&accX, &accY, &accZ);
     // Display our sensor data
+
 #ifdef DISPLAY_RAW
     M5.Lcd.setCursor(0, 50);
     M5.Lcd.printf(" %5.2f   %5.2f   %5.2f", accX, accY, accZ);
@@ -52,10 +53,16 @@ void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
     float roll = D180 * std::atan(accY / std::sqrt(accX * accX + accZ * accZ)) / M_PI;
     pitch_diff = pitch - message.pitch;
     float diff = pitch_diff - pitch_diff_set;
-    M5.Lcd.setCursor(0, 80);
-    M5.Lcd.printf("pitch_diff:     %5.3f", pitch_diff);
-    M5.Lcd.setCursor(0, 100);
-    M5.Lcd.printf("pitch_diff set: %5.3f", pitch_diff_set);
+//    M5.Lcd.setTextSize(2);
+//    M5.Lcd.setCursor(0, 80);
+//    M5.Lcd.printf("pitch_diff:     %5.3f", pitch_diff);
+//    M5.Lcd.setCursor(0, 100);
+//    M5.Lcd.printf("pitch_diff set: %5.3f", pitch_diff_set);
+
+    M5.Lcd.setTextSize(68);
+    M5.Lcd.setCursor(20, 100);
+    M5.Lcd.printf("%5.3f    ", pitch_diff_set - pitch_diff);
+
     if (roll > -ROL_TOLERANC && roll < ROL_TOLERANC && message.roll > -ROL_TOLERANC && message.roll < ROL_TOLERANC) {
         Say(diff, PITCH_TOLERANCE);
     }
