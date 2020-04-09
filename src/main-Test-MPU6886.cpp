97cd78bfdb59985d7254f6cea68f84880b88e0c9
andreas <andreas.luethi@gmx.net>
2020-04-09 13:33:28 +0200

    Experimenting.

--- src/main-Test-MPU6886.cpp
+++ src/main-Test-MPU6886.cpp
@@ -3,6 +3,7 @@
 #include <Arduino.h>
 #include <M5Stack.h>
 #include <Kalman.h>
+#include <Ewma.h> // https://github.com/jonnieZG/EWMA
 
 #define D180 180.0
 
@@ -19,6 +20,7 @@ uint32_t timer;
 double dt;
 
 Kalman kalmanX;
+Ewma filter(0.5); // 0.0 .. 1.0
 
 void setup() {
     Serial.begin(115200);
@@ -45,21 +47,21 @@ void loop() {
     M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
 
 
+    M5.Lcd.setTextSize(2);
     M5.Lcd.setCursor(0, 50);
     M5.Lcd.printf(" %5.3f   %5.3f   %5.3f", accX, accY, accZ);
 
-//    float yaw = D180 * std::atan(accZ / std::sqrt(accX * accX + accZ * accZ)) / M_PI;
-//    float pitch = D180 * std::atan(accX / std::sqrt(accY * accY + accZ * accZ)) / M_PI;
-//    float roll = D180 * std::atan(accY / std::sqrt(accX * accX + accZ * accZ)) / M_PI;
     float yaw;
     float pitch;
     float roll;
-    M5.IMU.getAhrsData(&pitch, &roll, &yaw);
+    yaw = D180 * std::atan(accZ / std::sqrt(accX * accX + accZ * accZ)) / M_PI;
+    pitch = D180 * std::atan(accX / std::sqrt(accY * accY + accZ * accZ)) / M_PI;
+    roll = D180 * std::atan(accY / std::sqrt(accX * accX + accZ * accZ)) / M_PI;
 
 //    float gyroXrate = gyroX / 131.0; // Convert to deg/s
 //    float kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
 //
-//
+    float filtered_pitch = filter.filter(pitch);
 
     M5.Lcd.setCursor(0, 80);
     M5.Lcd.printf("yaw:   % 5.3f", yaw);
@@ -67,6 +69,11 @@ void loop() {
     M5.Lcd.printf("pitch: % 5.3f", pitch);
     M5.Lcd.setCursor(0, 120);
     M5.Lcd.printf("roll:  % 5.3f", roll);
+
+    M5.Lcd.setTextSize(8);
+    M5.Lcd.setCursor(20, 180);
+    M5.Lcd.printf("%+03.3f    ", filtered_pitch);
+
 
 //    M5.Lcd.setCursor(180, 120);
 //    M5.Lcd.printf("% 5.2f", kalAngleX);
