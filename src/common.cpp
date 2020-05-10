3d849bed369a2d171f318909e9da6f526b5f362d
Your Name <you@example.com>
2020-05-10 13:10:09 +0200

    Optimize.

--- src/common.cpp
+++ src/common.cpp
@@ -1,28 +1,27 @@
 #include <cmath>
+#include <Arduino.h>
 #include "common.h"
 //
 // Created by andreas on 13.04.20.
 //
 
-
 float calcYay(const float accX, const float accY, const float accZ) {
     //D180 * atan(accZ / sqrt(accX * accX + accZ * accZ)) / M_PI;
     float t = accX * accX + accZ * accZ;
     if (t == 0.0) return t;
-    return D180 * std::atan(accZ / std::sqrt(t)) / M_PI;
+    return RAD_TO_DEG * std::atan(accZ / std::sqrt(t));
 }
 
 float calcPitch(const float accX, const float accY, const float accZ) {
     //  D180 * atan(accX / sqrt(accY * accY + accZ * accZ)) / M_PI;
     float t = accY * accY + accZ * accZ;
     if (t == 0.0) return t;
-    return D180 * std::atan(accX / std::sqrt(t)) / M_PI;
+    return RAD_TO_DEG * std::atan(accX / std::sqrt(t));
 }
 
 float calcRoll(const float accX, const float accY, const float accZ) {
     // D180 * atan(accY / sqrt(accX * accX + accZ * accZ)) / M_PI;
     float t = accX * accX + accZ * accZ;
-    if (t == 0.0)
-        return t;
-    return D180 * std::atan(accY / std::sqrt(t)) / M_PI;
+    if (t == 0.0) return t;
+    return RAD_TO_DEG * std::atan(accY / std::sqrt(t));
 }
