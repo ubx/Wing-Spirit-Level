1625523dc7b9945001c7cbca5306ebe85c57fa2c
Andreas Lüthi <andreas.luethi@gmx.net>
2020-07-08 13:51:21 +0200

    Add battery level indicator.

--- src/main-Test-MPU6886.cpp
+++ src/main-Test-MPU6886.cpp
@@ -1,4 +1,5 @@
 #define M5STACK_MPU6886
+#define BAT_TEST
 
 #include <Arduino.h>
 #include <M5Stack.h>
@@ -22,12 +23,37 @@ double dt;
 Kalman kalmanX;
 Ewma filter(0.5); // 0.0 .. 1.0
 
+#ifdef BAT_TEST
+
+int8_t getBatteryLevel() {
+    Wire.beginTransmission(0x75);
+    Wire.write(0x78);
+    if (Wire.endTransmission(false) == 0
+        && Wire.requestFrom(0x75, 1)) {
+        switch (Wire.read() & 0xF0) {
+            case 0xE0:
+                return 25;
+            case 0xC0:
+                return 50;
+            case 0x80:
+                return 75;
+            case 0x00:
+                return 100;
+            default:
+                return 0;
+        }
+    }
+    return -1;
+}
+
+#endif
+
 void setup() {
     Serial.begin(115200);
 
     M5.begin();
     M5.Power.begin();
-
+#ifndef BAT_TEST
     M5.IMU.Init();
     M5.IMU.setAccelFsr(M5.IMU.AFS_2G);  // MPU6886 sensor, ±2g
 
@@ -37,10 +63,23 @@ void setup() {
     M5.Lcd.setTextSize(4);
     M5.Lcd.print("Test MPU6886");
     M5.Lcd.setTextSize(2);
+#endif
 }
 
 void loop() {
-    dt = (double)(micros() - timer) / 1000000; // Calculate delta time
+
+
+#ifdef BAT_TEST
+    M5.Lcd.clear();
+    M5.Lcd.setCursor(40, 20);
+    M5.Lcd.setTextColor(GREEN);
+    M5.Lcd.setTextSize(2);
+    M5.Lcd.print("Battery Level: ");
+    M5.Lcd.print(getBatteryLevel());
+    M5.Lcd.print("%");
+    delay(10000);
+#else
+    dt = (double) (micros() - timer) / 1000000; // Calculate delta time
     timer = micros();
 
     M5.IMU.getAccelData(&accX, &accY, &accZ);
@@ -73,11 +112,11 @@ void loop() {
     M5.Lcd.setTextSize(8);
     M5.Lcd.setCursor(20, 180);
     M5.Lcd.printf("%+03.3f    ", filtered_pitch);
-
+    delay(500);
+#endif
 
 //    M5.Lcd.setCursor(180, 120);
 //    M5.Lcd.printf("% 5.2f", kalAngleX);
 
 
-    delay(500);
 }
